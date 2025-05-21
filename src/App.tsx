13193f694115e523825034569d2c5e7622d4a679
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-05-21 23:01:26 +0000

    montando o Cabecalho

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Banner from "./components/Banner";
+import Cabecalho from "./components/Cabecalho";
 import Link from "./components/Link";
 
 function App() {
   return (
     <>
+      <Cabecalho />
       <Banner src="/images/banner.png" alt="Banner do Luz e Cena" />
       <Link href="/" target="_blank">Link</Link>
     </>
--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import Logo from "../Logo";
+import CabecalhoAcoes from "./components/CabecalhoAcoes";
+import CabecalhoFormFiltro from "./components/CabecalhoFormFiltro";
+import CabecalhoLinks from "./components/CabecalhoLinks";
+import CabecalhoLista from "./components/CabecalhoLista";
+import CabecalhoListaItem from "./components/CabecalhoListaItem";
 
 const Cabecalho = () => {
-  return <div>Cabecalho</div>;
-};
+  return (
+    <header>
+      <CabecalhoLista>
+        <CabecalhoListaItem>
+          <Logo src="/Logo.png" alt="Logo do Luz e Cena." />
+        </CabecalhoListaItem>
+        <CabecalhoListaItem>
+          <CabecalhoLinks />
+        </CabecalhoListaItem>
+        <CabecalhoListaItem>
+          <CabecalhoFormFiltro />
+        </CabecalhoListaItem>
+        <CabecalhoListaItem>
+          <CabecalhoAcoes />
+        </CabecalhoListaItem>
+      </CabecalhoLista>
+    </header>
+  );
+}
 
 export default Cabecalho;
