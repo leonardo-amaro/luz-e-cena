a562b97fb18666d9eea4494f224192e4f5cbd098
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-05-23 21:34:36 +0000

    exibindo os filmes na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Banner from "./components/Banner";
 import Cabecalho from "./components/Cabecalho";
-import Link from "./components/Link";
+import SectionFilmes from "./components/SectionFilmes";
 
 function App() {
   return (
     <>
       <Cabecalho />
       <Banner src="/images/banner.png" alt="Banner do Luz e Cena" />
-      <Link href="/" target="_blank">Link</Link>
+      <SectionFilmes />
     </>
   );
 }
